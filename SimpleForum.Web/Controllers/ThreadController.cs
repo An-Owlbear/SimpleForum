117c129c49e49f5c43537330fff6ffb908c4127a
An-Owlbear <AnOwlbear@protonmail.com>
2020-10-16 22:11:15 +0100

    Updated ThreadController to use repository instead of DbContext

--- SimpleForum.Web/Controllers/ThreadController.cs
+++ SimpleForum.Web/Controllers/ThreadController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -14,31 +15,21 @@ namespace SimpleForum.Web.Controllers
 {
     public class ThreadController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        private readonly SimpleForumConfig _config;
+        private readonly SimpleForumRepository _repository;
         private int PostsPerPage = 30;
 
-        public ThreadController(ApplicationDbContext context, IOptions<SimpleForumConfig> config)
+        public ThreadController(SimpleForumRepository repository)
         {
-            _context = context;
-            _config = config.Value;
+            _repository = repository;
         }
         
-        public IActionResult Index(int? id, int page = 1)
+        // Returns a page of the requested thread
+        public async Task<IActionResult> Index(int id, int page = 1)
         {
-            if (id == null) return Redirect("/");
+            // Retrieves the requested thread
+            Thread thread = await _repository.GetThreadAsync(id);
 
-            Thread thread;
-
-            try
-            {
-                thread = _context.Threads.First(x => x.ThreadID == id);
-            }
-            catch (InvalidOperationException)
-            {
-                return StatusCode(404);
-            }
-
+            // Returns message of thread has been removed
             if (thread.Deleted || thread.User.Deleted)
             {
                 MessageViewModel messageModel = new MessageViewModel()
@@ -49,29 +40,25 @@ namespace SimpleForum.Web.Controllers
                 return View("Message", messageModel);
             }
             
-            User user = null;
-            if (User.Identity.IsAuthenticated)
-            {
-                int userID = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
-                user = _context.Users.First(x => x.UserID == userID);
-            }
+            // Sets the current user
+            User user = await _repository.GetUserAsync(User);
             
+            // Retrieves comments for the requested page
+            IEnumerable<Comment> comments = _repository.GetThreadReplies(thread, page);
+
+            // Creates model and returns view
             ThreadViewModel model = new ThreadViewModel()
             {
                 Thread = thread,
-                Comments = thread.Comments
-                    .Where(x => !x.Deleted && !x.User.Deleted)
-                    .OrderBy(x => x.DatePosted)
-                    .Skip((page - 1) * PostsPerPage)
-                    .Take(PostsPerPage),
+                Comments = comments,
                 Page = page,
                 PageCount = ((thread.Comments.Count + 1) + (PostsPerPage - 1)) / PostsPerPage,
                 User = user
             };
-
             return View("Thread", model);
         }
 
+        // Returns view for creating threads
         [Authorize]
         [ServiceFilter(typeof(VerifiedEmail))]
         [ServiceFilter(typeof(PreventMuted))]
@@ -80,15 +67,18 @@ namespace SimpleForum.Web.Controllers
             return View();
         }
         
+        // Creates a thread
         [Authorize]
         [ServiceFilter(typeof(VerifiedEmail))]
         [ServiceFilter(typeof(PreventMuted))]
         public async Task<IActionResult> CreateThread(string title, string content)
         {
+            // Redirects if the title or content are empty
             if (title == null || content == null) return Redirect("/Thread/Create");
 
+            // Creates and adds thread to database
             DateTime time = DateTime.Now;
-            int userID = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            int userID = Tools.GetUserID(User);
 
             Thread thread = new Thread()
             {
@@ -98,81 +88,60 @@ namespace SimpleForum.Web.Controllers
                 UserID = userID
             };
 
-            await _context.Threads.AddAsync(thread);
-            await _context.SaveChangesAsync();
+            await _repository.AddThreadAsync(thread);
+            await _repository.SaveChangesAsync();
 
+            // Redirects to thread
             int threadID = thread.ID;
             return Redirect("/Thread?id=" + threadID);
         }
         
+        // Posts a comment to a thread
         [Authorize(Policy = "ThreadReply")]
         [ServiceFilter(typeof(VerifiedEmail))]
         [ServiceFilter(typeof(PreventMuted))]
         public async Task<IActionResult> PostComment(string content, int threadID)
         {
-            // Retrieve user and threads
-            int userID = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
-            User user = await _context.Users.FindAsync(userID);
-            Thread thread = await _context.Threads.FindAsync(threadID);
-            
+            // Retrieves user
+            User user = await _repository.GetUserAsync(User);
+
             // Create and add comment
             Comment comment = new Comment()
             {
                 Content = content,
                 DatePosted = DateTime.Now,
                 ThreadID = threadID,
-                UserID = userID
+                UserID = user.UserID
             };
-            await _context.Comments.AddAsync(comment);
-            // Save changes so that comment ID can be accessed
-            await _context.SaveChangesAsync();
-
-
-            // Create notification if commenting on another user's thread
-            if (thread.UserID != userID)
-            {
-                Notification notification = new Notification()
-                {
-                    Title = $"{user.Username} left a comment on your post.",
-                    Content = $"Click [here]({_config.InstanceURL}/Thread?id={threadID}#{comment.ID}) to view.",
-                    DateCreated = DateTime.Now,
-                    UserID = thread.UserID
-                };
-                await _context.Notifications.AddAsync(notification);
-            }
+            await _repository.PostCommentAsync(comment);
+            await _repository.SaveChangesAsync();
             
-            // Saves changes and redirects
-            await _context.SaveChangesAsync();
+            // Redirects to comment
             return Redirect($"/Thread?id={threadID}#{comment.CommentID}");
         }
 
+        // Returns page for admin actions on threads
         [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AdminActions(int? id)
+        public async Task<IActionResult> AdminActions(int id)
         {
-            if (id == null) return Redirect("/");
-
-            // TODO - Add code for thread preview
-
-            Thread thread = await _context.Threads.FindAsync(id);
+            // Retrieves thread
+            Thread thread = await _repository.GetThreadAsync(id);
             
-            // Returns 404 if the thread was deleted by user
+            // Returns 404 if the thread was deleted by user, otherwise returns view
             if (thread.DeletedBy == "User") return NotFound();
-            
             return View(thread);
         }
-
-
-        [Authorize(Policy = "ThreadOwner")]
+        
+        // Deletes a thread
         [ServiceFilter(typeof(VerifiedEmail))]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int id)
         {
-            if (id == null) return Redirect("/");
-            Thread thread = _context.Threads.First(x => x.ThreadID == id);
-            thread.Deleted = true;
-            thread.DeletedBy = "User";
-            await _context.SaveChangesAsync();
+            // Deletes thread and saves changes
+            await _repository.DeleteThreadAsync(id, User);
+            await _repository.SaveChangesAsync();
             
+            // Creates model and returns view
             MessageViewModel model = new MessageViewModel()
             {
                 Title = "Thread deleted",
@@ -181,49 +150,25 @@ namespace SimpleForum.Web.Controllers
             return View("Message", model);
         }
 
-        // Page for admins submitting delete and reason
+        // Page for admins submitting thread deletion and reason
         [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AdminDelete(int? id)
+        public async Task<IActionResult> AdminDelete(int id)
         {
-            // Redirects if id is not set
-            if (id == null) return Redirect("/");
-            
-            // Retrieves thread and return 404 if already deleted by user
-            Thread thread = await _context.Threads.FindAsync(id);
-            if (thread.DeletedBy == "User") return NotFound();
-            
+            // Retrieves thread and returns view
+            Thread thread = await _repository.GetThreadAsync(id);
             return View(thread);
         }
         
         // Deletes a thread and provides a reason, sending a notification to the user
         [Authorize(Roles = "Admin")]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> SendAdminDelete(int? id, string reason)
+        public async Task<IActionResult> SendAdminDelete(int id, string reason)
         {
-            // Redirects if thread id is not set
-            if (id == null) return Redirect("/");
+            // Deletes thread and saves changes
+            await _repository.AdminDeleteThreadAsync(id, reason);
+            await _repository.SaveChangesAsync();
             
-            // Returns 404 if thread already deleted by user
-            Thread thread = _context.Threads.First(x => x.ThreadID == id);
-            if (thread.DeletedBy == "User") return NotFound();
-            
-            // Sets thread as deleted and delete reason
-            thread.Deleted = true;
-            thread.DeleteReason = reason;
-            thread.DeletedBy = "Admin";
-            
-            // Creates and adds notification
-            Notification notification = new Notification()
-            {
-                Title = $"Your thread '{thread.Title}' was deleted",
-                Content = $"Reason: {reason}",
-                DateCreated = DateTime.Now,
-                UserID = thread.UserID
-            };
-            await _context.Notifications.AddAsync(notification);
-            await _context.SaveChangesAsync();
-            
-            // Returns view
+            // Creates model and returns view
             MessageViewModel model = new MessageViewModel()
             {
                 Title = "Thread deleted",
@@ -232,16 +177,17 @@ namespace SimpleForum.Web.Controllers
             return View("Message", model);
         }
 
+        // Pins a thread to the top of the homepage
         [Authorize(Roles = "Admin")]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> Pin(int? id)
+        public async Task<IActionResult> Pin(int id)
         {
-            if (id == null) return Redirect("/");
-
-            Thread thread = _context.Threads.First(x => x.ThreadID == id);
+            // Retrieves and pins thread
+            Thread thread = await _repository.GetThreadAsync(id);
             thread.Pinned = true;
-            await _context.SaveChangesAsync();
+            await _repository.SaveChangesAsync();
 
+            // Creates model and returns view
             MessageViewModel model = new MessageViewModel()
             {
                 Title = "Thread pinned",
@@ -250,16 +196,17 @@ namespace SimpleForum.Web.Controllers
             return View("Message", model);
         }
 
+        // Locks a thread
         [Authorize(Roles = "Admin")]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> Lock(int? id)
+        public async Task<IActionResult> Lock(int id)
         {
-            if (id == null) return Redirect("/");
-
-            Thread thread = _context.Threads.First(x => x.ThreadID == id);
+            // Retrieves and locks thread
+            Thread thread = await _repository.GetThreadAsync(id);
             thread.Locked = true;
-            await _context.SaveChangesAsync();
+            await _repository.SaveChangesAsync();
 
+            // Creates model and returns view
             MessageViewModel model = new MessageViewModel()
             {
                 Title = "Thread locked",
@@ -268,17 +215,18 @@ namespace SimpleForum.Web.Controllers
             return View("Message", model);
         }
         
+        // Restores a thread of the given id
         [Authorize(Roles = "Admin")]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> Restore(int? id)
+        public async Task<IActionResult> Restore(int id)
         {
-            if (id == null) return Redirect("/");
-            Thread thread = _context.Threads.First(x => x.ThreadID == id);
-            if (thread.DeletedBy == "User") return NotFound();
-            
+            // Retrieves and restores thread
+            Thread thread = await _repository.GetThreadAsync(id);
             thread.Deleted = false;
-            await _context.SaveChangesAsync();
+            thread.DeletedBy = null;
+            await _repository.SaveChangesAsync();
             
+            // Creates model and returns view
             MessageViewModel model = new MessageViewModel()
             {
                 Title = "Thread restored",
@@ -287,16 +235,17 @@ namespace SimpleForum.Web.Controllers
             return View("Message", model);
         }
 
+        // Unpins a thread
         [Authorize(Roles = "Admin")]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> Unpin(int? id)
+        public async Task<IActionResult> Unpin(int id)
         {
-            if (id == null) return Redirect("/");
-
-            Thread thread = _context.Threads.First(x => x.ThreadID == id);
+            // Retrieves and unpins thread
+            Thread thread = await _repository.GetThreadAsync(id);
             thread.Pinned = false;
-            await _context.SaveChangesAsync();
+            await _repository.SaveChangesAsync();
 
+            // Creates model and returns view
             MessageViewModel model = new MessageViewModel()
             {
                 Title = "Thread unpinned",
@@ -305,16 +254,17 @@ namespace SimpleForum.Web.Controllers
             return View("Message", model);
         }
 
+        // Unlocks a thread
         [Authorize(Roles = "Admin")]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> Unlock(int? id)
+        public async Task<IActionResult> Unlock(int id)
         {
-            if (id == null) return Redirect("/");
-
-            Thread thread = _context.Threads.First(x => x.ThreadID == id);
+            // Retrieves and unlocks thread
+            Thread thread = await _repository.GetThreadAsync(id);
             thread.Locked = false;
-            await _context.SaveChangesAsync();
-
+            await _repository.SaveChangesAsync();
+            
+            // Creates model and returns view
             MessageViewModel model = new MessageViewModel()
             {
                 Title = "Thread unlocked",
@@ -323,17 +273,17 @@ namespace SimpleForum.Web.Controllers
             return View("Message", model);
         }
 
+        // Deletes a comment from a thread
         [Authorize(Policy = "CommentOwner")]
         [ServiceFilter(typeof(VerifiedEmail))]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> DeleteComment(int? id)
+        public async Task<IActionResult> DeleteComment(int id)
         {
-            if (id == null) return Redirect("/");
+            // Deletes the comment and saves changes
+            await _repository.DeleteCommentAsync(id, User);
+            await _repository.SaveChangesAsync();
 
-            Comment comment = _context.Comments.First(x => x.CommentID == id);
-            comment.Deleted = true;
-            await _context.SaveChangesAsync();
-
+            // Creates model and returns view
             MessageViewModel model = new MessageViewModel()
             {
                 Title = "Comment deleted",
@@ -341,42 +291,24 @@ namespace SimpleForum.Web.Controllers
             };
             return View("Message", model);
         }
-
+        
         // Page for admins deleting comments
         [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AdminDeleteComment(int? id)
+        public async Task<IActionResult> AdminDeleteComment(int id)
         {
-            // Redirects if id is not set
-            if (id == null) return Redirect("/");
-
             // Retrieves comment and returns view
-            Comment comment = await _context.Comments.FindAsync(id);
+            Comment comment = await _repository.GetCommentAsync(id);
             return View(comment);
         }
         
         // Deletes a comment as admin with a reason
         [Authorize(Roles = "Admin")]
         [ServiceFilter(typeof(CheckPassword))]
-        public async Task<IActionResult> SendAdminDeleteComment(int? id, string reason)
+        public async Task<IActionResult> SendAdminDeleteComment(int id, string reason)
         {
-            // Redirects if id is not set
-            if (id == null) return Redirect("/");
-
-            // Retrieves comment and deletes and sets reason
-            Comment comment = _context.Comments.First(x => x.CommentID == id);
-            comment.Deleted = true;
-            comment.DeleteReason = reason;
-            
-            // Creates and adds notification
-            Notification notification = new Notification()
-            {
-                Title = $"Your comment on {comment.Thread.Title} was deleted",
-                Content = $"Reason: {reason}",
-                DateCreated = DateTime.Now,
-                UserID = comment.UserID
-            };
-            await _context.Notifications.AddAsync(notification);
-            await _context.SaveChangesAsync();
+            // Deletes comment and saves changes
+            await _repository.AdminDeleteCommentAsync(id, reason);
+            await _repository.SaveChangesAsync();
 
             // Returns view
             MessageViewModel model = new MessageViewModel()
