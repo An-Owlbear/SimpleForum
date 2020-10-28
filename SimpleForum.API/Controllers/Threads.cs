bd7d0398340c56f1dcf7885fc45e8836c22bbf5b
An-Owlbear <AnOwlbear@protonmail.com>
2020-10-28 12:20:22 +0000

    Added threads page

---
+++ SimpleForum.API/Controllers/Threads.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleForum.API.Models;
+using SimpleForum.Internal;
+
+namespace SimpleForum.API.Controllers
+{
+    public class ThreadsController : ControllerBase
+    {
+        private readonly SimpleForumRepository _repository;
+
+        public ThreadsController(SimpleForumRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        [Route("Threads/")]
+        [HttpGet]
+        public async Task<IEnumerable<Thread>> Index(int page = 1)
+        {
+            IEnumerable<SimpleForum.Models.Thread> threads = await _repository.GetFrontPageAsync(page);
+            return threads.Select(x => new Thread(x));
+        }
+    }
+}
--- SimpleForum.API/Startup.cs
+++ SimpleForum.API/Startup.cs
@@ -60,18 +60,9 @@ namespace SimpleForum.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action}=Index/{id?}");
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
