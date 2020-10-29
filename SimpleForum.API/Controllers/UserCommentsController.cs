50f107d44f6c3fd57a475c205e654cfbaab05fb1
An-Owlbear <AnOwlbear@protonmail.com>
2020-10-29 11:04:18 +0000

    Added UserCommentsController

---
+++ SimpleForum.API/Controllers/UserCommentsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleForum.API.Models;
+using SimpleForum.Internal;
+
+namespace SimpleForum.API.Controllers
+{
+    [ApiController]
+    [Route("UserComments")]
+    public class UserCommentsController : Controller
+    {
+        private readonly SimpleForumRepository _repository;
+
+        public UserCommentsController(SimpleForumRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // Gets a UserComment of the given ID
+        [HttpGet("{id}")]
+        public async Task<Comment> GetUserComment(int id)
+        {
+            return new Comment(await _repository.GetUserCommentAsync(id));
+        }
+    }
+}
