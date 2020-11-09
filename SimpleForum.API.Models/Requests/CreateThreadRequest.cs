68d9d872e1c6d89ba131538bc5329845c535056f
An-Owlbear <AnOwlbear@protonmail.com>
2020-11-09 22:48:27 +0000

    Added CreateThread action to ThreadsController

---
+++ SimpleForum.API.Models/Requests/CreateThreadRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleForum.API.Models.Requests
+{
+    public class CreateThreadRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
--- SimpleForum.API/Controllers/ThreadsController.cs
+++ SimpleForum.API/Controllers/ThreadsController.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SimpleForum.API.Models.Requests;
 using SimpleForum.API.Models.Responses;
 using SimpleForum.Internal;
 
@@ -42,6 +45,30 @@ namespace SimpleForum.API.Controllers
         {
             IEnumerable<SimpleForum.Models.Comment> comments = await _repository.GetThreadRepliesAsync(id, page);
             return comments.Select(x => new Comment(x));
+        }
+
+        // Creates a new thread
+        [HttpPut("")]
+        [Authorize]
+        public async Task<IActionResult> CreateThread(CreateThreadRequest request)
+        {
+            // Returns error if either parameter is null
+            if (request.Title == null || request.Content == null) return NotFound("Title and content cannot be null");
+            
+            // Creates and adds thread to database
+            SimpleForum.Models.User user = await _repository.GetUserAsync(User);
+            SimpleForum.Models.Thread thread = new SimpleForum.Models.Thread()
+            {
+                Title = request.Title,
+                Content = request.Content,
+                DatePosted = DateTime.Now,
+                User = user
+            };
+            await _repository.AddThreadAsync(thread);
+            await _repository.SaveChangesAsync();
+            
+            // Returns JSON response
+            return Json(new Thread(thread));
         }
     }
 }
