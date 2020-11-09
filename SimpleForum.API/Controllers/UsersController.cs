5d0b0c12879f28846fc2bdc324dfe9180be9c11f
An-Owlbear <AnOwlbear@protonmail.com>
2020-11-09 23:17:44 +0000

    Added PostComment action to UsersController

--- SimpleForum.API/Controllers/UsersController.cs
+++ SimpleForum.API/Controllers/UsersController.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SimpleForum.API.Models.Requests;
 using SimpleForum.API.Models.Responses;
 using SimpleForum.Internal;
 
@@ -34,6 +37,34 @@ namespace SimpleForum.API.Controllers
         {
             IEnumerable<SimpleForum.Models.UserComment> comments = await _repository.GetUserCommentsAsync(id, page);
             return comments.Select(x => new Comment(x));
+        }
+        
+        // Posts a comment to a user's profile
+        [HttpPut("{id}/Comments")]
+        [Authorize]
+        public async Task<IActionResult> PostComment(int id, PostCommentRequest request)
+        {
+            // Returns error if comment is empty
+            if (request.Content == null) return BadRequest("Comment cannot be null");
+
+            // Retrieves user and return not found if thread not found
+            SimpleForum.Models.User currentUser = await _repository.GetUserAsync(User);
+            SimpleForum.Models.User profileUser = await _repository.GetUserAsync(id);
+            if (profileUser == null) return NotFound("Requested user not found");
+            
+            // Creates user comment and adds it to database
+            SimpleForum.Models.UserComment userComment = new SimpleForum.Models.UserComment()
+            {
+                Content = request.Content,
+                DatePosted = DateTime.Now,
+                User = currentUser,
+                UserPage = profileUser
+            };
+            await _repository.PostUserCommentAsync(userComment);
+            await _repository.SaveChangesAsync();
+            
+            // Returns JSON response
+            return Json(new Comment(userComment));
         }
     }
 }
