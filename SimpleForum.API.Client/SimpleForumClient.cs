f8b559fcaae99d73849655fe48c5f94f8b28f1a4
An-Owlbear <AnOwlbear@protonmail.com>
2020-12-13 18:22:00 +0000

    Added method for retrieving threads from the front page

--- SimpleForum.API.Client/SimpleForumClient.cs
+++ SimpleForum.API.Client/SimpleForumClient.cs
@@ -1,7 +1,22 @@
-﻿namespace SimpleForum.API.Client
+﻿using System.Text.Json;
+
+namespace SimpleForum.API.Client
 {
-    public class SimpleForumClient
+    public partial class SimpleForumClient
     {
-        
+        private string _fqdn;
+
+        private readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        private readonly RequestsClient _requestsClient;
+
+        public SimpleForumClient(string fqdn)
+        {
+            _fqdn = fqdn;
+            _requestsClient = new RequestsClient(_fqdn);
+        }
     }
 }
---
+++ SimpleForum.API.Client/Threads.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using SimpleForum.API.Models.Responses;
+
+namespace SimpleForum.API.Client
+{
+    public partial class SimpleForumClient
+    {
+        /// <summary>
+        /// Retrieves the list of threads at the front page
+        /// </summary>
+        /// <returns>The list of threads</returns>
+        public async Task<List<ApiThread>> GetFrontPage()
+        {
+            // Retrieves response, converts to a Stream, JsonDocument, and then list of threads
+            HttpResponseMessage response = await _requestsClient.SendRequest(Endpoints.FrontPage).ConfigureAwait(false);
+            Stream streamResult = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+            JsonDocument jsonResult = await JsonDocument.ParseAsync(streamResult).ConfigureAwait(false);
+            return JsonSerializer.Deserialize<List<ApiThread>>(jsonResult.RootElement.GetRawText(), jsonOptions);
+        }
+    }
+}
