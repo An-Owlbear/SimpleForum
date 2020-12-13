f02ed3c0e183b617cf9ebfa517b9a54603658434
An-Owlbear <AnOwlbear@protonmail.com>
2020-12-13 18:22:33 +0000

    Added tests for API client

---
+++ SimpleForum.API.Client.Tests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SimpleForum.API.Models.Responses;
+
+namespace SimpleForum.API.Client.Tests
+{
+    class Program
+    {
+        // TODO - Add more tests and test selection
+        static async Task Main(string[] args)
+        {
+            SimpleForumClient client = new SimpleForumClient("http://localhost:5002");
+            List<ApiThread> response = await client.GetFrontPage();
+
+            foreach (ApiThread thread in response)
+            {
+                Console.WriteLine($"Title - {thread.Title}\n" +
+                                  $"Content - {thread.Content}\n" +
+                                  $"Pinned - {thread.Pinned}\n" +
+                                  $"Locked - {thread.Locked}\n" +
+                                  $"Replies - {thread.Replies}\n");
+            }
+        }
+    }
+}
