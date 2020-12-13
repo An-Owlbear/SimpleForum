4f6be1631d55dc1ff8e7d182147849f61c6e99c0
An-Owlbear <AnOwlbear@protonmail.com>
2020-12-13 18:26:36 +0000

    Added page parameter to GetFrontPage method

--- SimpleForum.API.Client/Threads.cs
+++ SimpleForum.API.Client/Threads.cs
@@ -13,10 +13,15 @@ namespace SimpleForum.API.Client
         /// Retrieves the list of threads at the front page
         /// </summary>
         /// <returns>The list of threads</returns>
-        public async Task<List<ApiThread>> GetFrontPage()
+        public async Task<List<ApiThread>> GetFrontPage(int page = 1)
         {
+            Dictionary<string, string> parameters = new Dictionary<string, string>()
+            {
+                { "page", page.ToString() }
+            };
+            
             // Retrieves response, converts to a Stream, JsonDocument, and then list of threads
-            HttpResponseMessage response = await _requestsClient.SendRequest(Endpoints.FrontPage).ConfigureAwait(false);
+            HttpResponseMessage response = await _requestsClient.SendRequest(Endpoints.FrontPage, parameters).ConfigureAwait(false);
             Stream streamResult = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
             JsonDocument jsonResult = await JsonDocument.ParseAsync(streamResult).ConfigureAwait(false);
             return JsonSerializer.Deserialize<List<ApiThread>>(jsonResult.RootElement.GetRawText(), jsonOptions);
