14f8e4c15403b8f7eb5760a3a5176b778de2035d
An-Owlbear <AnOwlbear@protonmail.com>
2020-12-13 18:21:23 +0000

    Added SendRequest method to RequestsClient

--- SimpleForum.API.Client/RequestsClient.cs
+++ SimpleForum.API.Client/RequestsClient.cs
@@ -1,17 +1,22 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace SimpleForum.API.Client
 {
     public class RequestsClient
     {
-        private readonly ITokenStorage _tokenStorage;
         private HttpClient _client;
+        private string _fqdn;
 
-        public RequestsClient(ITokenStorage tokenStorage)
+        public RequestsClient(string fqdn)
         {
-            // Sets token storage
-            _tokenStorage = tokenStorage;
+            // Sets FQDN
+            _fqdn = fqdn;
             
             // Configures and creates HttpClient
             HttpClientHandler handler = new HttpClientHandler();
@@ -19,7 +24,47 @@ namespace SimpleForum.API.Client
             _client = new HttpClient(handler);
             _client.DefaultRequestHeaders.Add("Connection", "keep-alive");
         }
-        
-        
+
+        public async Task<HttpResponseMessage> SendRequest(Endpoint endpoint, Dictionary<string, string> parameters = null)
+        {
+            // Sets parameters as an empty dictionary if null
+            parameters ??= new Dictionary<string, string>();
+            
+            // Creates request
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Method = endpoint.Method;
+
+            // Creates a list of parameters in the url path
+            Dictionary<string, string> pathParameters = parameters
+                .Where(x => Regex.IsMatch(endpoint.Path, $@":{x.Key}\W"))
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            // Creates a list of remaining parameters
+            Dictionary<string, string> remainingParams = parameters
+                .Except(pathParameters)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            // Creates url string
+            string url = _fqdn + pathParameters
+                .Aggregate(endpoint.Path,
+                    (acc, next) =>
+                        Regex.Replace(acc, $@":{next.Key}(?!\w)", next.Value));
+
+            // adds parameters to request
+            if (request.Method == HttpMethod.Get)
+            {
+                UriBuilder address = new UriBuilder(url);
+                address.Query = await new FormUrlEncodedContent(remainingParams).ReadAsStringAsync().ConfigureAwait(false);
+                request.RequestUri = new Uri(address.ToString());
+            }
+            else
+            {
+                request.RequestUri = new Uri(url);
+                request.Content = new FormUrlEncodedContent(remainingParams);
+            }
+
+            // Returns response
+            return await _client.SendAsync(request).ConfigureAwait(false);
+        }
     }
 }
