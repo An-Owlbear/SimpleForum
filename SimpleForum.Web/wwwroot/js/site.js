84816ad8b9ae7e8aea56445d55ac922843e36336
An-Owlbear <AnOwlbear@protonmail.com>
2021-01-12 19:13:35 +0000

    Updated navigation bar to be mobile responsive

--- SimpleForum.Web/wwwroot/js/site.js
+++ SimpleForum.Web/wwwroot/js/site.js
@@ -1,38 +1,38 @@
 ﻿function openDropdown(object) {
-    object.classList.add("dropdown-clicked");
+    object.classList.add('dropdown-clicked');
     object.removeEventListener('click', object.openDropdown);
     object.addEventListener('click', object.closeDropdown);
 
-    let dropdownItems = object.getElementsByClassName("dropdown-items");
+    let dropdownItems = object.getElementsByClassName('dropdown-items');
     Array.from(dropdownItems).forEach(function (element) {
-        element.classList.add("dropdown-items-visible");
+        element.classList.add('dropdown-items-visible');
     })
 }
 
 function closeDropdown(object) {
-    object.classList.remove("dropdown-clicked");
+    object.classList.remove('dropdown-clicked');
     object.removeEventListener('click', object.closeDropdown);
     object.addEventListener('click', object.openDropdown);
 
-    let dropdownItems = object.getElementsByClassName("dropdown-items");
+    let dropdownItems = object.getElementsByClassName('dropdown-items');
     Array.from(dropdownItems).forEach(function (element) {
-        element.classList.remove("dropdown-items-visible");
+        element.classList.remove('dropdown-items-visible');
     })
 }
 
-let dropdowns = document.getElementsByClassName("dropdown");
+let dropdowns = document.getElementsByClassName('dropdown');
 
 Array.from(dropdowns).forEach(function (element) {
     element.openDropdown = () => openDropdown(element);
     element.closeDropdown = () => closeDropdown(element);
-    element.addEventListener("click", element.openDropdown);
+    element.addEventListener('click', element.openDropdown);
 })
 
 // Crops the image if it is not square
 function cropImage(image, outputImage) {
     // Gets the canvas and calculates values
-    let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext("2d");
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
     let diff = Math.abs(image.width - image.height);
     let crop = Math.ceil(diff / 2);
     
@@ -51,13 +51,30 @@ function cropImage(image, outputImage) {
 
 // Shows a preview of the new profile picture after chosen
 function updateProfileEdit() {
-    let profileImg = document.getElementById("profilepicture-upload").files[0];
-    let profileElement = document.getElementById("profilepicture-preview");
-    let tempImage = document.getElementById("tempImage");
+    let profileImg = document.getElementById('profilepicture-upload').files[0];
+    let profileElement = document.getElementById('profilepicture-preview');
+    let tempImage = document.getElementById('tempImage');
     let reader = new FileReader();
     reader.onload = function(e) {
         tempImage.src = e.target.result;
         tempImage.onload = () => cropImage(tempImage, profileElement);
     }
     reader.readAsDataURL(profileImg);
-} 
+}
+
+// Opens and closes the navigation bar
+function toggleMenu(element, nav) {
+    if (element.classList.contains("open")){
+        nav.scrollTo(0,0);
+        element.classList.remove("open");
+    }
+    else {
+        element.classList.add("open");
+    }
+}
+
+// Adds event listener to navigation bar
+let navbar = document.querySelector('.navbar');
+let navbarNav = document.querySelector('.navbar-nav');
+let button = document.querySelector('.navbar .toggle-menu')
+button.addEventListener('click', () => toggleMenu(navbar, navbarNav));
