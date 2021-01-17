685f262b250f32764c89076f111643a10181b5af
An-Owlbear <AnOwlbear@protonmail.com>
2021-01-17 23:53:13 +0000

    Refactored string parsing methods

--- TextParser/StandardParsers.fs
+++ TextParser/StandardParsers.fs
@@ -6,6 +6,10 @@ let internal letters = [for i in 65..90 -> char i] @ [for i in 97..122 -> char i
 
 let internal charListToString charList =
     String(List.toArray charList)
+    
+// String parsing functions
+let parseString0 parser = parseMany0 parser |>> charListToString
+let parseString1 parser = parseMany1 parser |>> charListToString
 
 // Parses a whitespace character
 let parseWhitespaceChar =
@@ -13,20 +17,20 @@ let parseWhitespaceChar =
     parse condition
     
 // Parses zero or more whitespace characters
-let spaces0 = parseMany0 parseWhitespaceChar
+let spaces0 = parseString0 parseWhitespaceChar
 
 // Parses one or more whitespace characters
-let spaces1 = parseMany1 parseWhitespaceChar
+let spaces1 = parseString1 parseWhitespaceChar
 
 // Functions for parsing letters
 let parseLetter = anyOf letters
-let parseLetters0 = parseMany0 parseLetter |>> charListToString
-let parseLetters1 = parseMany1 parseLetter |>> charListToString
+let parseLetters0 = parseString0 parseLetter
+let parseLetters1 = parseString1 parseLetter
     
 // Functions for parsing letters/numbers
 let parseLetterOrNumber =
     letters @ [for i in 48..57 -> char i]
     |> anyOf
     
-let parseLettersOrNumbers0 = parseMany0 parseLetterOrNumber |>> charListToString
-let parseLettersOrNumbers1 = parseMany1 parseLetterOrNumber |>> charListToString
+let parseLettersOrNumbers0 = parseString0 parseLetterOrNumber
+let parseLettersOrNumbers1 = parseString1 parseLetterOrNumber
