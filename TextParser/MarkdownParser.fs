1a1410bbe8796827a9ff7cb3d553bf754dc371c8
An-Owlbear <AnOwlbear@protonmail.com>
2021-01-17 23:53:37 +0000

    Added WIP markdown parsing

---
+++ TextParser/MarkdownParser.fs
@@ -0,0 +1,20 @@
+﻿module TextParser.MarkdownParser
+open ParserCombinators
+open StandardParsers
+
+type MarkdownValue =
+    | Bold of string
+    | Italic of string
+    | Heading of int * string
+    | Link of string * string
+    | Image of string * string
+    | BlockQuote of string
+    | List of string list
+    
+let bold =
+    let asterisk = parseChar '*'
+    let quote = asterisk .>>. asterisk
+    let condition ch = (ch <> '*')
+    let charParser = parse condition
+    quote >>. parseString0 charParser .>> quote
+    |>> Bold
