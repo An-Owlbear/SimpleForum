1e5b4405308cfb42cb74213e9d492c77fa724ada
An-Owlbear <AnOwlbear@protonmail.com>
2021-01-18 22:22:50 +0000

    Added project to test parser

---
+++ TextParser.Tests/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open TextParser.MarkdownParser
+
+[<EntryPoint>]
+let main argv =
+    let result = parseMarkdown "**asdasdasd *asdasda* **"
+    printfn "%A" result
+    0 // return an integer exit code
