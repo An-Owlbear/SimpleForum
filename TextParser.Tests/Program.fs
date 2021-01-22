7100ee641f6375e2aa2dc2e59ea3878557539ced
An-Owlbear <AnOwlbear@protonmail.com>
2021-01-22 18:36:36 +0000

    Updated tests for markdown parsing, including comparison with popular markdown parsing library

--- TextParser.Tests/Program.fs
+++ TextParser.Tests/Program.fs
@@ -1,9 +1,43 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿open System.Diagnostics
+open System.IO
+open System.Text
+open FSharp.Formatting.Markdown
+open TextParser.ParserCombinators
 open TextParser.MarkdownParser
+
+let generateFile content =
+    [
+        "<!DOCTYPE html>"
+        "<head>"
+        "\t<title>Markdown test</title>"
+        "</head>"
+        "<body>"
+        sprintf "\t%s" content
+        "</body>"
+        "</html>"
+    ] |> List.fold (fun (sb : StringBuilder) -> sb.Append) (StringBuilder())
+    |> fun x -> x.ToString()
+
 
 [<EntryPoint>]
 let main argv =
-    let result = parseMarkdown "* adas  **asdasdasd**  dasd *"
-    printfn "%A" result
-    0 // return an integer exit code
+    let timer = Stopwatch()
+
+    let text = File.ReadAllText("Sample.md")
+    
+    timer.Start()
+    let result = parseMarkdown text
+    
+    match result with
+    | Success (value, _) -> File.WriteAllText("Sample.html", generateFile (markdownToHTML value))
+    | Failure _ -> printfn "Failure"
+    timer.Stop()
+    printfn "%f" timer.Elapsed.TotalMilliseconds
+    
+    timer.Restart()
+    let parsed = Markdown.Parse(text)
+    let html = Markdown.ToHtml(parsed)
+    File.WriteAllText("OtherSample.html", html)
+    timer.Stop()
+    printfn "%f" timer.Elapsed.TotalMilliseconds    
+    0
---
+++ TextParser.Tests/Sample.md
@@ -0,0 +1,11 @@
+﻿# This is a heading
+
+This is some normal text.
+
+**This is some bold text**
+
+*This is some italic text*
+
+## This is a subheading
+
+This is some **mixed** text *with* **different *formatting* in it.**
