a264bffc2ff70d5d3377949e6a4c2aa9093c4a29
An-Owlbear <AnOwlbear@protonmail.com>
2021-01-28 09:47:20 +0000

    Added other URLs to config

--- SimpleForum.Common.Server/SimpleForumConfig.cs
+++ SimpleForum.Common.Server/SimpleForumConfig.cs
@@ -4,6 +4,8 @@ namespace SimpleForum.Common.Server
     {
         public string InstanceName { get; set; }
         public string InstanceURL { get; set; }
+        public string APIURL { get; set; }
+        public string CrossConnectionURL { get; set; }
         public bool RequireEmailVerification { get; set; }
         public string PrivateKey { get; set; }
         public int WebPort { get; set; }
