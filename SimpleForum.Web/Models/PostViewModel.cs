8473d3d15d893580a8ddb3db8889f3d199753aed
An-Owlbear <AnOwlbear@protonmail.com>
2021-03-16 21:40:52 +0000

    Fixed buttons for deleting comments

--- SimpleForum.Web/Models/PostViewModel.cs
+++ SimpleForum.Web/Models/PostViewModel.cs
@@ -5,6 +5,15 @@ namespace SimpleForum.Web.Models
     public class PostViewModel
     {
         public IPost Post { get; set; }
-        public string ID { get; set; } 
+        public string ID { get; set; }
+        public PostType PostType { get; set; }
+        public User CurrentUser { get; set; }
+    }
+
+    public enum PostType
+    {
+        ThreadPost,
+        Comment,
+        UserComment
     }
 }
